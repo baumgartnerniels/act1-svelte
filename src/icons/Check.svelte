5e57a3cfa069e72507b583b297291b34ee7003ba
Niels Baumgartner <baumgartnerniels@gmail.com>
2024-10-22 10:19:18 +0200

    change icons

---
+++ src/icons/Check.svelte
@@ -0,0 +1,12 @@
+<svg
+  class="check"
+  xmlns="http://www.w3.org/2000/svg"
+  version="1.1"
+  viewBox="0 0 24 24"
+>
+  <g>
+    <g id="Layer_1">
+      <path d="M9,16.4L22,3.4l2,2-15,15L0,11.4l2-2,7,7Z" />
+    </g>
+  </g>
+</svg>
